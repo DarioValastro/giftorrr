c22ae160b3c06deec4b2fb293878bab77e599c24
Dario <dario.valastro@gmail.com>
2020-10-30 20:45:11 +0100

    Create navigation bar in home.html linked to routes

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/home/')
+def home():
+    return render_template('home.html')
 
+@app.route('/contactUs/')
+def contact_Us():
+    return render_template('contactUs.html')
+
+
+@app.route('/aboutUs/')
+def about_Us():
+    return render_template('aboutUs.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
